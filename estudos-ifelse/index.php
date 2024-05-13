422ddb9bb9a475be21a242e8cf2159672471ae24
lucasbritosimoa <lucas@simoa.com>
2024-05-13 12:57:46 -0300

    Update index.php
    Criei o arquivo base do estudo if, else e elseif. Agora, vou começar a codar e fazer as anotações.

---
+++ estudos-ifelse/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudos if ELSE</title>
+    <style>
+        body {
+            background: linear-gradient(to right, #EAE8E3, #83E6E9);
+            font-family: arial;
+            color: #00000;
+            font-size: 25px;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <h2>Estudos sobre IF, ELSE e ELSEIF</h2>
+    <section id="formulario">
+
+    </section>
+    <main>
+
+        <?php
+        echo "Olá Mundo!";
+
+        ?>
+    </main>
+
+</body>
+
+</html>
