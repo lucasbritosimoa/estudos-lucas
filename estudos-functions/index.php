3d120066597e3c9e01326f29f578df361c6510a9
lucasbritosimoa <lucas@simoa.com>
2024-05-14 12:35:37 -0300

    Create index.php

---
+++ estudos-functions/index.php
@@ -0,0 +1,42 @@
+<section id="php">
+    <?php 
+    
+    ?>
+</section>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudos - functions</title>
+    <style>
+        body {
+            background: linear-gradient(to right, #EAE8E3, #83E6E9);
+            font-family: arial;
+            color: #000;
+            font-size: 25px;
+            text-align: center;
+            line-height: 1.5em;
+        }
+    </style>
+</head>
+
+<body>
+    <h2>Estudos sobre funções</h2>
+    <section id="formulario">
+
+    </section">
+    <p>
+    <main  id="resultado">
+
+        <?php
+        echo "Olá Mundo!";
+
+        ?>
+    </main>
+    </p>
+</body>
+
+</html>
